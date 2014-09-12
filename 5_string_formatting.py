9192289771339db5c7420ffbb598e8ed2a3d97fe
Benjamin Bergman <ben@benbergman.ca>
2014-09-12 13:21:08 -0500

    Add string formatting section

---
+++ 5_string_formatting.py
@@ -0,0 +1,11 @@
+name = "John"
+print("Hello, %s!" % name)
+
+age = 23
+print("%s is %d year sold." % (name, age))
+
+mylist = [1,2,3]
+print("A list: %s" % mylist)
+
+balance = 53.44444
+print("Hello %s, age %d. Your current balance is $%.2f." % (name, age, balance))
