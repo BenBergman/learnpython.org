81ffec600d90ccd56451ce545403878dc12caf1f
Benjamin Bergman <ben@benbergman.ca>
2014-09-12 13:10:46 -0500

    Finished basic operator section

--- 4_basic_operators.py
+++ 4_basic_operators.py
@@ -1,3 +1,32 @@
 number = 1 + 2 * 3 / 4.0
-
 print(number)
+
+remainder = 11 % 3
+print(remainder)
+
+squared = 7 ** 2
+print(squared)
+
+cubed = 2 ** 3
+print(cubed)
+
+helloworld ="hello" + " " + "world"
+print(helloworld)
+
+lotsofhellos = "hello" * 10
+print(lotsofhellos)
+
+evens = [2,4,6,8]
+odds = [1,3,5,7]
+alls = odds + evens
+print(alls)
+
+oddss = odds * 3
+print(oddss)
+
+x = "yolo"
+y = "broseph"
+x_list = [x] * 10
+y_list = [y] * 10
+big_list = x_list + y_list
+print(big_list)
